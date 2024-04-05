b7a0d86ff0639011ae65638559ce42c0b429db8f
Murillo Ribeiro <162358376+MuRibeiroDev@users.noreply.github.com>
2024-04-05 20:46:10 -0300

    DataTest

---
+++ src/OOP/DataTest.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class DataTest {
+    public static void main(String[] args) {
+    Data data = new Data(15,10,2005);
+
+        System.out.println("Data: ");
+        data.formatData();
+    }
+
+}
